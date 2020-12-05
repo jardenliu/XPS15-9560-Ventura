0e135841316437c54c7cc683e5c0ec990283d5c3
jardenliu <15915839707@163.com>
2020-12-05 20:36:39 +0800

    feat: add build script

---
+++ make-i7-1080p.sh
@@ -0,0 +1,19 @@
+# var
+FILE_NAME="XPS15-9560-1080P"
+
+# clear 
+rm -rf ./.dist
+
+# create dir
+mkdir -p .dist
+
+# copy config && tools
+cp -rf OC .dist/
+cp -rf Post-install .dist/
+
+# 
+sed -i '' 's/AAPL00,override-no-connect/#AAPL00,override-no-connect/g' .dist/OC/config.plist
+
+# pack
+ mv .dist $FILE_NAME
+ zip -r -m $FILE_NAME-OC.zip  $FILE_NAME
